ec6dea63ba3420a57b5657d752a8716843000ba4
ayato_p <ayato.skycrawlers@gmail.com>
2016-03-20 17:31:09 +0900

    Implement hubot component :tada:

--- hubot-component/src/hubotify_cljs/component/hubot.cljs
+++ hubot-component/src/hubotify_cljs/component/hubot.cljs
@@ -1,8 +1,53 @@
 (ns hubotify-cljs.component.hubot
-  (:require [com.stuartsierra.component :as component]))
+  "Reload idea from:
+  https://github.com/vinta/hubot-reload-scripts/blob/master/src/reload-scripts.coffee"
+  (:require-macros [fence.core :refer [+++]])
+  (:require [cljs.nodejs :as nodejs]
+            [com.stuartsierra.component :as component]))
+
+
+(def fs (nodejs/require "fs"))
+(def path (nodejs/require "path"))
 
 (defprotocol Reloadable
   (reload [this]))
+
+(defn- clear-all-commands! [robot]
+  (aset robot "commands" #js []))
+
+(defn- clear-all-listeners! [robot]
+  (aset robot "listeners" #js []))
+
+(defn- load-scripts! [robot]
+  (+++ (let [script-path (.resolve path "." "scripts")]
+         (.load robot script-path))))
+
+(defn- load-src-scripts! [robot]
+  (+++ (let [script-path (.resolve path "." "src" "scripts")]
+         (.load robot script-path))))
+
+(defn- load-hubot-scripts! [robot]
+  (+++ (let [hubot-script-path (.resolve path "." "hubot-scripts.json")]
+         (.readFile fs hubot-script-path #js {:encoding "UTF-8"}
+                    (fn [err data]
+                      (if (> (.-length data) 0)
+                        (try
+                          (let [scripts (.parse js/JSON data)
+                                scripts-path (.resolve path "node_modules" "hubot-scripts" "src" "scripts")]
+                            (.loadHubotScripts robot scripts-path scripts))
+                          (catch js/Error err
+                            (.error js/console "Error parsing JSON data from hubot-scripts.json: " err)))))))))
+
+(defn- load-external-scripts! [robot]
+  (+++ (let [external-scripts (.resolve path "." "external-scripts.json")]
+         (.readFile fs external-scripts #js {:encoding "UTF-8"}
+                    (fn [err data]
+                      (if (> (.-length data) 0)
+                        (try
+                          (let [scripts (.parse js/JSON data)]
+                            (.loadExternalScripts robot scripts))
+                          (catch js/Error err
+                            (.error js/console "Error parsing JSON data from external-scripts.json: " err)))))))))
 
 (defrecord Hubot [robot listeners]
   component/Lifecycle
@@ -13,7 +58,16 @@
 
   Reloadable
   (reload [_]
-    (.-commands robot)))
+    ;; Clear all commands/listeners
+    (clear-all-commands! robot)
+    (clear-all-listeners! robot)
+
+    ;; Load all scripts
+    (load-scripts! robot)
+    (load-src-scripts! robot)
+    (load-hubot-scripts! robot)
+    (load-external-scripts! robot)
+    (+++ (.info js/console "Reloaded all scripts"))))
 
 (defn new-hubot [robot]
   (map->Hubot {:robot robot}))
